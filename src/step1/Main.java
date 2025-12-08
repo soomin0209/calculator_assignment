19a093b8127c0da065d25f9686a151521f3b0859
soomin0209 <soomin02094@gmail.com>
2025-12-08 14:02:24 +0900

    feat: 두 정수를 사칙연산 기호에 따라 계산하고 오류 출력하는 기능 추가 (step1)

--- src/step1/Main.java
+++ src/step1/Main.java
@@ -23,5 +23,31 @@ public class Main {
 
         System.out.print("사칙연산 기호를 입력하세요: ");
         char op = sc.next().charAt(0);
+
+        int result = 0;
+        switch (op) {
+            case '+':
+                result = num1 + num2;
+                System.out.println("결과: " + result);
+                break;
+            case '-':
+                result = num1 - num2;
+                System.out.println("결과: " + result);
+                break;
+            case '*':
+                result = num1 * num2;
+                System.out.println("결과: " + result);
+                break;
+            case '/':
+                try {
+                    result = num1 / num2;
+                    System.out.println("결과: " + result);
+                } catch (ArithmeticException e) {
+                    System.out.println("나눗셈 연산에서 분모가 0이 입력될 수 없습니다.");
+                }
+                break;
+            default:
+                System.out.println("잘못된 연산 기호 입니다.");
+        }
     }
 }
