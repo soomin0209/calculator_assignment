547ba8dd336e173a5181d7baf65b9ab6bf7f5e26
soomin0209 <soomin02094@gmail.com>
2025-12-08 15:06:34 +0900

    feat: exit 입력 전까지 사칙연산 무한 반복 기능 추가 (step1)

--- src/step1/Main.java
+++ src/step1/Main.java
@@ -10,44 +10,57 @@ public class Main {
         System.out.print("첫 번째 정수를 입력해주세요: ");
         int num1 = sc.nextInt();
         while (num1 < 0) {
-            System.out.print("다시 입력해주세요: ");
+            System.out.print("0 이상의 정수로 다시 입력해주세요: ");
             num1 = sc.nextInt();
         }
 
         System.out.print("두 번째 정수를 입력해주세요: ");
         int num2 = sc.nextInt();
         while (num2 < 0) {
-            System.out.print("다시 입력해주세요: ");
+            System.out.print("0 이상의 정수로 다시 입력해주세요: ");
             num2 = sc.nextInt();
         }
 
-        System.out.print("사칙연산 기호를 입력하세요: ");
-        char op = sc.next().charAt(0);
+        while (true) {
+            System.out.print("\n사칙연산 기호를 입력하세요(+, -, *, /, 종료:exit): ");
+            String input = sc.next();
+            if (input.equals("exit")) {
+                break;
+            }
 
-        int result = 0;
-        switch (op) {
-            case '+':
-                result = num1 + num2;
-                System.out.println("결과: " + result);
-                break;
-            case '-':
-                result = num1 - num2;
-                System.out.println("결과: " + result);
-                break;
-            case '*':
-                result = num1 * num2;
-                System.out.println("결과: " + result);
-                break;
-            case '/':
-                try {
-                    result = num1 / num2;
+            if (input.length() != 1) {
+                System.out.println("*** 연산 기호는 한 글자만 입력해주세요 ***");
+                continue;
+            }
+
+            char op = input.charAt(0);
+
+            int result = 0;
+            switch (op) {
+                case '+':
+                    result = num1 + num2;
                     System.out.println("결과: " + result);
-                } catch (ArithmeticException e) {
-                    System.out.println("나눗셈 연산에서 분모가 0이 입력될 수 없습니다.");
-                }
-                break;
-            default:
-                System.out.println("잘못된 연산 기호 입니다.");
+                    break;
+                case '-':
+                    result = num1 - num2;
+                    System.out.println("결과: " + result);
+                    break;
+                case '*':
+                    result = num1 * num2;
+                    System.out.println("결과: " + result);
+                    break;
+                case '/':
+                    try {
+                        result = num1 / num2;
+                        System.out.println("결과: " + result);
+                    } catch (ArithmeticException e) {
+                        System.out.println("*** 나누는 수는 0이 입력될 수 없습니다 ***");
+                    }
+                    break;
+                default:
+                    System.out.println("*** 잘못된 연산 기호 입니다 ***");
+                    break;
+            }
         }
     }
 }
