3ce65f65e7c61e46e61ea1dfb44701abea5ff535
soomin0209 <soomin02094@gmail.com>
2025-12-08 11:44:39 +0900

    feat: 양의 정수 두 개 입력 받고 음수면 다시 입력하도록 기능 추가 (step1)

---
+++ src/step1/Main.java
@@ -0,0 +1,25 @@
+package step1;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("첫 번째 정수를 입력해주세요: ");
+        int num1 = sc.nextInt();
+        while (num1 < 0) {
+            System.out.print("다시 입력해주세요: ");
+            num1 = sc.nextInt();
+        }
+
+        System.out.print("두 번째 정수를 입력해주세요: ");
+        int num2 = sc.nextInt();
+        while (num2 < 0) {
+            System.out.print("다시 입력해주세요: ");
+            num2 = sc.nextInt();
+        }
+
+    }
+}
